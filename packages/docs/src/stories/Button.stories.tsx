2a1ea4e97d644db046b9b204716298cd9a4c0a4d
yurigarrido <y.garrido.yuri@gmail.com>
2023-06-27 20:41:10 -0300

    feat: add action on component

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -8,6 +8,11 @@ export default {
   args: {
     children: 'Send',
   },
+  argTypes: {
+    onClick: {
+      acttion: 'click',
+    },
+  },
 } as Meta<ButtonProps>
 
 export const Primary: StoryObj<ButtonProps> = {}
