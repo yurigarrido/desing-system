f2756b7fc9a8dec6a224c62ad9246d246f5119cf
yurigarrido <y.garrido.yuri@gmail.com>
2023-06-28 21:41:47 -0300

    fix: add sub path to storybook on production

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/design-system/'
+      config.base = '/desing-system/'
     }
 
     return config
